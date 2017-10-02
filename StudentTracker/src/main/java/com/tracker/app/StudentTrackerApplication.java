fbce8a9579b85c95bb623025a50b6e5768d61b88
kkane106 <kkane106@gmail.com>
2017-10-02 10:47:03 -0600

    Add SpringBoot project Add .gitignore file

---
+++ StudentTracker/src/main/java/com/tracker/app/StudentTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.tracker.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTrackerApplication.class, args);
+	}
+}
