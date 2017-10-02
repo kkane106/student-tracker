41e028776312a50ee93db9bc33615e7fe5587248
AndrewConlin <andrew.c.conlin@gmail.com>
2017-10-02 15:14:48 -0600

    Added Entity/Repo/Test for note

---
+++ StudentTracker/src/main/java/com/tracker/app/entities/Note.java
@@ -0,0 +1,46 @@
+package com.tracker.app.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Note {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String topic;
+	
+	private String content;
+
+	//private Employee employee;
+
+	//gets and sets
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
