14a39b4bf3a02864f73f9f7f24050f3c6eef6bb2
AndrewConlin <andrew.c.conlin@gmail.com>
2017-10-02 14:53:29 -0600

    Added Entity/Repo/Mappings for Contact

---
+++ StudentTracker/src/test/java/com/tracker/app/ContactTest.java
@@ -0,0 +1,29 @@
+package com.tracker.app;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.tracker.app.entities.Contact;
+import com.tracker.app.repo.ContactRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContactTest {
+	@Autowired
+	private ContactRepository contactRepo;
+	
+	@Test
+	public void test_cohort_mappings() {
+		Contact contact = contactRepo.findOne(1);
+		assertEquals(1, contact.getId());
+		assertEquals("Test", contact.getFname());
+		assertEquals("Test", contact.getLname());
+		assertEquals("test@test.com", contact.getEmail());
+		assertEquals("(555)3038682", contact.getPhone());
+	}
+}
